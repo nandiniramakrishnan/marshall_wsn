a5da9155a878a5f8dc31a1264472ab374925e0ef
Nandini Ramakrishnan <nramakri@andrew.cmu.edu>
2017-04-02 18:36:51 +0000

    ready for demo forreal

--- client1.py
+++ client1.py
@@ -7,7 +7,7 @@ import drive_fcns as DF
 import RPi.GPIO as GPIO
 
 #initialize client0 at location (0,0) facing East
-curr_row = 0
+curr_row = 1
 curr_col = 0
 curr_orient = "E" 
 
@@ -20,7 +20,7 @@ def wanna_quit(queue):
 # This is the target function of all DRIVING threads. Only DRIVING to happen here.
 # Communication with DRIVING thread will happen with argument "queue".
 # This function will call line following (all sensing and actuation code)
-def drive(row, col, queue, curr_row, curr_col, curr_orient):
+def drive(row, col, queue, curr_row, curr_col, curr_orient, sock):
     row = int(row)
     col = int(col)
     print("row %d" % row)
@@ -35,47 +35,65 @@ def drive(row, col, queue, curr_row, curr_col, curr_orient):
     while (curr_col != col):   
 
         if (path['E'] > 0):
-	        if (DF.line_follow(curr_orient, "E") == 0):
-	            path['E'] = path['E']-1	
-   	            curr_col = curr_col + 1
-		    curr_orient = "E"
-		    queue.put((curr_row, curr_col, curr_orient))    	        
-		else:
-		        print("went off grid, mission failed")
-		        return
+            if (DF.line_follow(curr_orient, "E") == 0):
+                path['E'] = path['E']-1	
+                curr_col = curr_col + 1
+            else:
+                print("went off grid, mission failed")
+                return
+            curr_orient = "E"
+            while not queue.empty():
+                queue.get()
+            queue.put((curr_row, curr_col, curr_orient))    	        
+            new_buf = [ 'P', str(curr_row), str(curr_col) ]
+            new_msg = ''.join(new_buf)
+            sock.sendall(new_msg)
 
         elif (path['W'] > 0):
-	        if (DF.line_follow(curr_orient, "W") == 0):
-	            path['W'] = path['W']-1
-	            curr_col = curr_col-1
-		    curr_orient = "W"
-		    queue.put((curr_row, curr_col, curr_orient))
-	        else:
-		        print("went off grid, mission failed")
-		        return
+            if (DF.line_follow(curr_orient, "W") == 0):
+                path['W'] = path['W']-1
+                curr_col = curr_col-1
+            else:
+                print("went off grid, mission failed")
+                return
+            curr_orient = "W"
+            while not queue.empty():
+                queue.get()
+            queue.put((curr_row, curr_col, curr_orient))    	        
+            new_buf = [ 'P', str(curr_row), str(curr_col) ]
+            new_msg = ''.join(new_buf)
+            sock.sendall(new_msg)
     
     while (curr_row != row):
         if (path['N'] > 0):
-	        if (DF.line_follow(curr_orient, "N") == 0):
-	    	    path['N'] = path['N']-1
-	            curr_row = curr_row-1
-		    curr_orient = "N"
-		    queue.put((curr_row, curr_col, curr_orient))
-    
-	        else:
-		        print("went off grid, mission failed")
-		        return
+            if (DF.line_follow(curr_orient, "N") == 0):
+                path['N'] = path['N']-1
+                curr_row = curr_row-1
+            else:
+                print("went off grid, mission failed")
+                return
+            curr_orient = "N"
+            while not queue.empty():
+                queue.get()
+            queue.put((curr_row, curr_col, curr_orient))    	        
+            new_buf = [ 'P', str(curr_row), str(curr_col) ]
+            new_msg = ''.join(new_buf)
+            sock.sendall(new_msg)
     
         elif (path['S'] > 0):
-	        if (DF.line_follow(curr_orient, "S") == 0):
-	            path['S'] = path['S']-1
-	            curr_row = curr_row+1
-		    curr_orient = "S"
-		    queue.put((curr_row, curr_col, curr_orient))
-  
-	        else:
-		        print("went off grid, mission failed")
-		        return
+            if (DF.line_follow(curr_orient, "S") == 0):
+                path['S'] = path['S']-1
+                curr_row = curr_row+1
+            else:
+                print("went off grid, mission failed")
+                return
+            curr_orient = "S"
+            while not queue.empty():
+                queue.get()
+            queue.put((curr_row, curr_col, curr_orient))    	        
+            new_buf = [ 'P', str(curr_row), str(curr_col) ]
+            new_msg = ''.join(new_buf)
+            sock.sendall(new_msg)
 
 
     # Send update to main thread for transmitting to Marshall
@@ -132,22 +150,23 @@ try:
                     # Send "ready for path" message to Marshall
                     # Receive directions and keep pushing into drive_comms_queue
                     
-                    thread = Thread(target = drive, args = (drive_row, drive_col, drive_comms_queue, curr_row, curr_col, curr_orient))
+                    thread = Thread(target = drive, args = (drive_row, drive_col, drive_comms_queue, curr_row, curr_col, curr_orient, sock))
                     driving = True
-
                     thread.start()
-		    print("thread started")
+                    print("thread started")
                     thread.join()
-		    print("thread joined")
+                    print("thread joined")
                     driving = False
-		if not drive_comms_queue.empty():
-		    new_pos = drive_comms_queue.get()
-                    curr_row = new_pos[0]
-                    curr_col = new_pos[1]
-                    curr_orient = new_pos[2]
-	            new_buf = [ 'P', str(curr_row), str(curr_col) ]
-	            new_msg = ''.join(new_buf)
-	            sock.sendall(new_msg)
+		    
+            #if not drive_comms_queue.empty():
+            #    print "the queue isn't empty!!"
+                new_pos = drive_comms_queue.get()
+                curr_row = new_pos[0]
+                curr_col = new_pos[1]
+                curr_orient = new_pos[2]
+            #    new_buf = [ 'P', str(curr_row), str(curr_col) ]
+            #    new_msg = ''.join(new_buf)
+            #    sock.sendall(new_msg)
 finally:
     print 'Closing socket'
     sock.close()
