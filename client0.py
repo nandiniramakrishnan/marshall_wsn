71f97a133a8194231b4159ac4d354975acb062ee
Nandini Ramakrishnan <nramakri@andrew.cmu.edu>
2017-04-07 19:28:35 +0000

    fixed self

--- client0.py
+++ client0.py
@@ -5,9 +5,10 @@ from threading import Thread
 import Queue
 import drive_fcns as DF
 import RPi.GPIO as GPIO
+import os
 
 # Server address
-server_address = ('128.237.202.197', 10000)
+server_address = ('128.237.160.61', 10000)
 
 # This is the target function of all DRIVING threads. Only DRIVING to happen here.
 # Communication with DRIVING thread will happen with argument "queue".
@@ -18,13 +19,13 @@ class DriverThread(Thread):
         self.curr_row = curr_row
         self.curr_col = curr_col
         self.curr_orient = curr_orient
-        self.dest_row = int(row)
-        self.dest_col = int(col)
+        self.dest_row = int(dest_row)
+        self.dest_col = int(dest_col)
         self.queue = queue
 
     def run(self):
         # Obtain directions to the destination and store in path
-        path = DF.path_plan(curr_row, curr_col, self.row, self.col)
+        path = DF.path_plan(self.curr_row, self.curr_col, self.dest_row, self.dest_col)
 
         #follow path to destination
         #follows E/W and then N/S
@@ -45,11 +46,11 @@ class DriverThread(Thread):
                     print("went off grid, mission failed")
                     return
                 self.curr_orient = "W"
-            queue.put((self.curr_row, self.curr_col, self.curr_orient))
+            self.queue.put((self.curr_row, self.curr_col, self.curr_orient))
 
-        while (self.curr_row != dest_row):
+        while (self.curr_row != self.dest_row):
             if (path['N'] > 0):
-                if (DF.line_follow(curr_orient, "N") == 0):
+                if (DF.line_follow(self.curr_orient, "N") == 0):
                     path['N'] = path['N']-1
                     self.curr_row = self.curr_row - 1
                 else:
@@ -57,15 +58,92 @@ class DriverThread(Thread):
                     return
                 self.curr_orient = "N"
             elif (path['S'] > 0):
-                if (DF.line_follow(curr_orient, "S") == 0):
+                if (DF.line_follow(self.curr_orient, "S") == 0):
                     path['S'] = path['S']-1
                     self.curr_row = self.curr_row + 1
                 else:
                     print("went off grid, mission failed")
                     return
                 self.curr_orient = "S"
-            queue.put((self.curr_row, self.curr_col, self.curr_orient))
-        return
+            self.queue.put((self.curr_row, self.curr_col, self.curr_orient))
+        print "drivings done in drivethread"
+	return
+
+
+    def runTrial(self):
+        # Obtain directions to the destination and store in path
+        path = DF.path_plan(self.curr_row, self.curr_col, self.dest_row, self.dest_col)
+
+        #follow path to destination
+        #follows E/W and then N/S
+        while (self.curr_col != self.dest_col):
+            if (path['E'] > 1):
+                if (DF.line_follow(self.curr_orient, "E") == 0):
+                    path['E'] = path['E']-1	
+                    self.curr_col = self.curr_col + 1
+                    self.next_col = self.curr_col + 1
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "E"
+            elif (path['W'] > 1):
+                if (DF.line_follow(self.curr_orient, "W") == 0):
+                    path['W'] = path['W'] - 1
+                    self.curr_col = self.curr_col - 1
+                    self.next_col = self.curr_col - 1
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "W"
+            elif (path['E'] == 1):
+                if (DF.line_follow(self.curr_orient, "E") == 0):
+                    path['E'] = path['E'] - 1
+                    self.curr_col = self.curr_col + 1
+                    if (path['N'] > 0):
+                        self.next_row = self.curr_row - 1
+                    elif (path['S'] > 0):
+                        self.next_row = self.curr_row + 1
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "W"
+            elif (path['W'] == 1):
+                if (DF.line_follow(self.curr_orient, "W") == 0):
+                    path['W'] = path['W'] - 1
+                    self.curr_col = self.curr_col - 1
+                    if (path['N'] > 0):
+                        self.next_row = self.curr_row - 1
+                    elif (path['S'] > 0):
+                        self.next_row = self.curr_row + 1
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "W"
+            self.queue.put((self.curr_row, self.curr_col, self.curr_orient, self.next_row, self.next_col))
+
+
+        while (self.curr_row != self.dest_row):
+            if (path['N'] > 1):
+                if (DF.line_follow(self.curr_orient, "N") == 0):
+                    path['N'] = path['N']-1
+                    self.curr_row = self.curr_row - 1
+                    self.next_row = self.curr_row - 1 
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "N"
+            elif (path['S'] > 1):
+                if (DF.line_follow(self.curr_orient, "S") == 0):
+                    path['S'] = path['S']-1
+                    self.curr_row = self.curr_row + 1
+                    self.next_row = self.next_row + 1
+                else:
+                    print("went off grid, mission failed")
+                    return
+                self.curr_orient = "S"
+            self.queue.put((self.curr_row, self.curr_col, self.curr_orient, self.next_row, self.next_col))
+        print "drivings done in drivethread"
+	return
 
 class Node:
     def __init__(self, node_id, drivingState, curr_row, curr_col, curr_orient):
@@ -91,7 +169,7 @@ class Node:
         
         # Send CHK message to reveal yourself to the Marshall
         print 'Sending "%s"' % chk_msg
-        sock.sendall(chk_msg)
+        self.sock.sendall(chk_msg)
         print "Waiting for ACK..."
 
         # Look for the ACK from marshall
@@ -102,8 +180,6 @@ class Node:
                 print 'Received "%s"' % data
     
         while True:
-            # Listen data
-            data = sock.recv(16)
             # You received a command!
             if data != None and len(data) == 3:
                 print 'Received "%s"' % data
@@ -112,7 +188,8 @@ class Node:
                 command_queue.put((dest_row, dest_col))
 
             if self.drivingState == False and not command_queue.empty():
-                (dest_row, dest_col) = command_queue.get()
+                print "gonna start driving!"
+		(dest_row, dest_col) = command_queue.get()
                 drivingThread = DriverThread(self.curr_row, self.curr_col, self.curr_orient, dest_row, dest_col, drive_comms_queue)
                 self.thread_list.append(drivingThread)
                 self.drivingState = True
@@ -120,12 +197,14 @@ class Node:
 
             for thread in self.thread_list:
                 if not thread.isAlive():
+		    print "threads dead"
                     self.drivingState = False
                     self.thread_list.remove(thread)
                     thread.join()
 
             if not drive_comms_queue.empty():
-                new_pos = drive_comms_queue.get()
+                print "drive queue is not empty!"
+		new_pos = drive_comms_queue.get()
                 self.curr_row = new_pos[0]
                 self.curr_col = new_pos[1]
                 self.curr_orient = new_pos[2]
@@ -133,6 +212,8 @@ class Node:
                 new_msg = ''.join(new_buf)
                 self.sock.sendall(new_msg)
     
+            # Listen data
+            data = self.sock.recv(16)
         print 'Closing socket'
         self.sock.close()
         motors.setSpeeds(0, 0)
