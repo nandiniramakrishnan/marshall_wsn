fb28d96319ee7d114cd872efcf7b15d528f391fc
Nandini Ramakrishnan <nramakri@andrew.cmu.edu>
2017-04-11 19:52:44 +0000

    added prox sensor text func

---
+++ prox_sensor_test.py
@@ -0,0 +1,25 @@
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+CLK  = 5
+MISO = 6
+MOSI = 24  
+CS   = 25
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Main program loop.
+while True:
+    #read prox sensor value
+    #prox sensor is on adc input 0
+    raw_val = mcp.read_adc(0)
+    if (raw_val > 11):
+        true_val = 2076/(raw_val-11)
+    else:
+	true_val = 0
+    print(true_val)
+    time.sleep(0.5)
